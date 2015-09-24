8ace917dfbf934780881431efce06624ea5590c7
Josh Long <josh@joshlong.com>
2015-09-24 09:49:33 +0200

    cleanup

--- labs/6/cf-simple.sh
+++ labs/6/cf-simple.sh
@@ -2,14 +2,16 @@
 cd config-service
 cf push
 # run cf apps to get the URL of config-service,
-# note it and place it in the next line where it says _URL_
+# or note it from the "urls" output of the `cf push`
+# and place it in the next line where it says _URL_
 cf cups config-service -p '{"uri":"http://_URL_"}'
 cd ..
 
 cd eureka-service
 cf push
 # run cf apps to get the URL of eureka-service,
-# note it and place it in the next line where it says _URL_
+# or note it from the "urls" output of the `cf push`,
+# and place it in the next line where it says _URL_
 cf cups eureka-service -p '{"uri":"http://_URL_"}'
 cd ..
 
