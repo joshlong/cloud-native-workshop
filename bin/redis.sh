1f4585da0fa0b73faa7b4e67851d3505725d061b
Josh Long <josh@joshlong.com>
2015-12-18 09:45:58 -0500

    better redis.sh

--- bin/redis.sh
+++ bin/redis.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker run --name redis -p 6379:6379   redis
+
+redis_container_id=$( docker ps -a | grep redis | cut -f1 -d\ )
+
+
+if [ "$redis_container_id" = "" ] ;
+then
+  docker run --name redis -p 6379:6379 redis
+else
+  echo "we found a container called 'redis' with this container ID: $redis_container_id."
+  docker start $redis_container_id
+fi
