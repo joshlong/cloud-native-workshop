bfcf68ab1d15fd91c1254e6d8b2aec30c8a6d02e
Mike Götz <mike.goetz@ipoint-systems.de>
2017-04-07 22:40:44 +0200

    Version updates and updated .gitignore to exclude eclipse files

--- code/auth-service/src/main/java/com/example/AuthServiceApplication.java
+++ code/auth-service/src/main/java/com/example/AuthServiceApplication.java
@@ -49,6 +49,46 @@ class Account {
 		this.username = u;
 		this.password = p;
 	}
+
+	public Long getId () {
+		return id;
+	}
+
+	
+	public void setId ( Long id ) {
+		this.id = id;
+	}
+
+	
+	public String getUsername () {
+		return username;
+	}
+
+	
+	public void setUsername ( String username ) {
+		this.username = username;
+	}
+
+	
+	public String getPassword () {
+		return password;
+	}
+
+	
+	public void setPassword ( String password ) {
+		this.password = password;
+	}
+
+	
+	public boolean isActive () {
+		return active;
+	}
+
+	
+	public void setActive ( boolean active ) {
+		this.active = active;
+	}
+	
 }
 
 interface AccountRepository extends JpaRepository<Account, Long> {
